171b1201d34048b8866aa0fda3942dd5c1c1967e
silviuvulcan <silviuvulcan@users.noreply.github.com>
2018-08-17 19:41:18 +0300

    added suport to set sensor

--- check_sshpftemp.sh
+++ check_sshpftemp.sh
@@ -7,6 +7,8 @@ function usage {
     echo "   Print detailed usage information"
     echo "-H, --host"
     echo "   Hostname where to run wl"
+    echo "-S, --sensor"
+    echo "   Sensor to read temp eg: dev.cpu.0.temperature or dev.amdtemp.0.core0.sensor0"
     echo "-W, --warn"
     echo "   Warning temperature level"
     echo "-C, --crit"
@@ -29,6 +31,9 @@ while [ "$1" != "" ]; do
         -H | --host )           shift
                                 HOST=$1
                                 ;;
+        -S | --sensor )         shift
+                                SENSOR=$1
+                                ;;
         -C | --crit )           shift
                                 CRIT=$1
                                 ;;
@@ -44,52 +49,57 @@ while [ "$1" != "" ]; do
     shift
 done
 
-if [ -z "$HOST" ]; then 
-	echo "Missing host parameter"
-	echo "See $0 --help for usage information"
-	exit ${STATE_UNKNOWN}
+if [ -z "$HOST" ]; then
+        echo "Missing host parameter"
+        echo "See $0 --help for usage information"
+        exit ${STATE_UNKNOWN}
 fi
-if [ -z "$CRIT" ]; then 
-	echo "Missing critical parameter"
-	echo "See $0 --help for usage information"
-	exit ${STATE_UNKNOWN}
+if [ -z "$SENSOR" ]; then
+        echo "Missing sensor parameter"
+        echo "See $0 --help for usage information"
+        exit ${STATE_UNKNOWN}
 fi
-if [ -z "$WARN" ]; then 
-	echo "Missing warning parameter"
-	echo "See $0 --help for usage information"
-	exit ${STATE_UNKNOWN}
+if [ -z "$CRIT" ]; then
+        echo "Missing critical parameter"
+        echo "See $0 --help for usage information"
+        exit ${STATE_UNKNOWN}
+fi
+if [ -z "$WARN" ]; then
+        echo "Missing warning parameter"
+        echo "See $0 --help for usage information"
+        exit ${STATE_UNKNOWN}
 fi
 
 if ! [[ $WARN =~ $numre ]]; then
-	echo "Warning level $WARN is not an integer"
-	exit ${STATE_UNKNOWN}
+        echo "Warning level $WARN is not an integer"
+        exit ${STATE_UNKNOWN}
 fi
 if ! [[ $CRIT =~ $numre ]]; then
-	echo "Critical level $CRIT is not an integer"
-	exit ${STATE_UNKNOWN}
+        echo "Critical level $CRIT is not an integer"
+        exit ${STATE_UNKNOWN}
 fi
 if [ $CRIT -lt $WARN ]; then
-	echo "Critical level $CRIT is lower than the warning level $WARN"
-	exit ${STATE_UNKNOWN}
+        echo "Critical level $CRIT is lower than the warning level $WARN"
+        exit ${STATE_UNKNOWN}
 fi
 
-RES=$(ssh $HOST "sysctl dev.amdtemp.0.core0.sensor0")
+RES=$(ssh $HOST "sysctl ${SENSOR}")
 if [ $? -ne 0 ]; then
-	echo "SSH connection failed"
-	exit ${STATE_UNKNOWN}
+        echo "SSH connection failed"
+        exit ${STATE_UNKNOWN}
 fi
 CTMP=$(echo $RES |awk '{ print $2 }'|tr -d C)
 CTMPINT=$(echo $CTMP | awk -F. '{print $1}')
 
 if [ $CTMPINT -lt $WARN ]; then
-	echo "Temperature OK: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
-	exit ${STATE_OK}
+        echo "Temperature OK: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
+        exit ${STATE_OK}
 elif [ $CTMPINT -ge $WARN ]&&[ $CTMPINT -lt $CRIT ]; then
-	echo "Temperature WARNING: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
-	exit ${STATE_WARNING}
+        echo "Temperature WARNING: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
+        exit ${STATE_WARNING}
 else
-	echo "Temperature CRITICAL: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
-	exit ${STATE_CRITICAL}
+        echo "Temperature CRITICAL: ${CTMP}$(awk 'BEGIN { print "\xc2\xb0"; }')C | temp=$CTMP;$WARN;$CRIT;;"
+        exit ${STATE_CRITICAL}
 fi
 echo "Something went wrong. Debug: crit $CRIT, warn $WARN, temp $CTMP, ssh $RES"
 exit ${STATE_UNKNOWN}
